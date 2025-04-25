471f3e07bbd4fc4542cd3d42fe1f4d8146660e80
KenpsSouza <kenpsrossi@gmail.com>
2025-04-24 21:19:32 -0300

    limpando cod testes

--- src/calculadora_salario_refatorado/descontos/Desconto.java
+++
@@ -1,9 +0,0 @@
-
-package calculadora_salario_refatorado.descontos;
-
-/**
- * Interface para cálculo de descontos.
- */
-public interface Desconto {
-    double calcular(double salarioBruto, int dependentes);
-}
--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,4 @@
  * 
  */
 module calculadora_salario {
-    requires java.desktop;
 }
